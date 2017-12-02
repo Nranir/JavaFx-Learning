fd6028ff74a542aa49d31bafaa26c941ad7365f7
Marcin <nranir@gmail.com>
2017-12-02 20:11:02 +0100

    lesson 001

---
+++ 001.JavaFx Introduction/src/lesson1/Notepad.java
@@ -0,0 +1,31 @@
+package lesson1;
+
+public class Notepad
+{
+	/*
+	 * What is JavaFx?
+	 * 
+	 * -A media grapthics framework for creating GUI in javapplications
+	 * FXML - new xml bases markup language for defining ui
+	 * 
+	 * -abiliti to apply CSS
+	 * -3d support
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * stage javafx.stage (window)
+	 * {
+	 *  Scene jsvafx.scene
+	 *  {
+	 *  
+	 *  	stackpane root
+	 *  	button btn
+	 *  }
+	 * 
+	 * }
+	 * 
+	 */
+}
