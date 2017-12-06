42a8c9a7002e5af520b5abb80ba4100009a31828
Marcin <nranir@gmail.com>
2017-12-06 19:06:43 +0100

    lesson 7 -8

---
+++ 007.StylinWithCSS/src/application/MainController.java
@@ -0,0 +1,20 @@
+package application;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+import java.util.Random;
+public class MainController
+{
+	@FXML //to pomaga znajdowanie rzeczy w scene builderze
+	private Label myMessage; 
+	@FXML
+	public void generateRandom(ActionEvent event)  //uwaga to musi byc javafx Actionevent
+	{
+		Random rand =  new Random();
+		int myrand = rand.nextInt(50)+1;
+		myMessage.setText(Integer.toString(myrand));
+		//System.out.println(Integer.toString(myrand));
+	}
+}
---
+++ 008.SimpleCalculator/src/application/Model.java
@@ -0,0 +1,11 @@
+package application;
+
+public class Model
+{
+	public float Calulate(long number1, long number2, String operator)
+	{
+		
+		
+		return 0;
+	}
+}
