1e614cd75a0827c36c4ac8c26af3471cf95d59f3
Marcin <nranir@gmail.com>
2017-12-09 18:10:09 +0100

    Update of calulator

--- 008.SimpleCalculator/src/application/Model.java
+++ 008.SimpleCalculator/src/application/Model.java
@@ -4,7 +4,33 @@ public class Model
 {
 	public float Calulate(long number1, long number2, String operator)
 	{
-		
+		switch (operator)
+		{
+			case "+":
+			{
+				return number1 + number2;
+			}
+			case "-":
+			{
+				return number1 - number2;
+			}
+			case "*":
+			{
+				return number1 * number2;
+			}
+			case "/":
+			{
+				if(number2==0)
+				{
+					return 0;
+				}
+				else
+				{
+					return ((float)number1 / (float)number2);
+				}
+				
+			}
+		}
 		
 		return 0;
 	}
